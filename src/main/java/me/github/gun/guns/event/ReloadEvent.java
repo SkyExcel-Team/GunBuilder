7b938c625cddc1cc82fc0565bd86f57c3cd10b6b
YooInseo <inseo0121@gmail.com>
2022-09-18 22:39:57 +0900

    ReloadEvent

---
+++ src/main/java/me/github/gun/guns/event/ReloadEvent.java
@@ -0,0 +1,45 @@
+package me.github.gun.guns.event;
+
+import me.github.gun.guns.module.Gun;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class ReloadEvent extends Event implements Cancellable {
+
+    private Gun gun;
+    private Player player;
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+    private boolean isCancelled;
+
+
+    public ReloadEvent(Gun gun, Player player) {
+        this.gun = gun;
+        this.player = player;
+    }
+
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.isCancelled = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public Gun getGun() {
+        return gun;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
